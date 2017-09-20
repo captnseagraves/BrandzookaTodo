68974a8f7d3c8ab9238b0e82ec253e2004557ef1
Kevin Seagraves <k.s.seagraves@gmail.com>
2017-09-20 13:46:22 -0600

    facebook share brandzooka.com working

--- components/App.js
+++ components/App.js
@@ -9,6 +9,7 @@ const App = () => (
     <AddTodo />
     <VisibleTodoList />
     <Footer />
+<div className="fb-share-button" data-href="https://brandzooka.com/register" data-layout="button" data-size="large" data-mobile-iframe="true"><a className="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fbrandzooka.com%2Fregister&amp;src=sdkpreparse">Share</a></div>
   </div>
 )
 
